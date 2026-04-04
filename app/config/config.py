8b6f15ec1268c4df61ddf1e7d22302f47516c355
mendozajhonny619-alt <mendozajhonny619@gmail.com>
2026-04-04 17:53:03 -0400

    Create config.py

---
+++ app/config/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY", "")
+    THE_ODDS_API_KEY = os.getenv("THE_ODDS_API_KEY", "")
+
+    CUOTA_MINIMA = 1.50
+    CUOTA_MAXIMA = 2.10
+
+    CONFIANZA_MINIMA = 75.0
+    EDGE_MINIMO = 0.05
+
+    VENTANAS_PRIORITARIAS = [(25, 45), (60, 75)]
+
+    SHADOW_MODE = False
