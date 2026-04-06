95b509c9086fe62e979a0d83255e69961c99bc2a
mendozajhonny619-alt <mendozajhonny619@gmail.com>
2026-04-06 09:52:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from flask import Flask
-from app.api.routes import register_routes
 
 app = Flask(__name__)
-register_routes(app)
 
-# 👇 IMPORTANTE PARA RENDER
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/")
+def home():
+    return "OK"
