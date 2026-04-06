b9fa9e8455ea6a9d8217a30557cdd7092425ca59
mendozajhonny619-alt <mendozajhonny619@gmail.com>
2026-04-06 12:24:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="JHONNY ELITE V16 API")
 
 @app.get("/")
 def root():
     return {"message": "API funcionando 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
